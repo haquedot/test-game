50425cb4ffa1c183e1f91bb3c6b9724a2d13cfd8
Meraj <haquedot@gmail.com>
2024-04-01 13:31:17 +0530

    test game

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import IdentifyFruit from './components/IdentifyFruit'
+
+function App() {
+
+  return (
+    <>
+      <IdentifyFruit />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/IdentifyFruit.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function IdentifyFruit() {
+    return (
+        <>
+            <iframe src="../games/AnimalGame/index.html" title="Identify Fruit Game" style={{ height: "100vh", width: "100vw", border: "none", margin: "none", overflow: "hidden" }} />
+        </>
+    );
+}
+
+export default IdentifyFruit;
